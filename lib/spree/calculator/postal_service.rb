3e75c3b1087f315f1ea5cfc033ff43d6034b3ceb
lostboy <paul.crabtree@gmail.com>
2012-07-01 19:51:03 +0100

    allow mass assignment for preferences

--- lib/spree/calculator/postal_service.rb
+++ lib/spree/calculator/postal_service.rb
@@ -10,6 +10,9 @@ class Spree::Calculator::PostalService < Spree::Calculator
   preference :handling_fee, :decimal, :default => 10
   preference :default_weight, :decimal, :default => 1
   
+  attr_accessible :preferred_weight_table, :preferred_price_table, :preferred_max_item_weight, :preferred_max_item_width,
+    :preferred_max_item_length, :preferred_min_price, :preferred_handling_max, :preferred_handling_fee, :preferred_default_weight
+  
   def self.description
     "Postal"
 #    I18n.t("postal_service")
